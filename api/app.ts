b2017d4e008ac76dc09cba6c04f4f8ab9e9572f4
Robin Louis van Komen <robin@vkmedia.dk>
2019-06-04 18:41:42 +0200

    add express to api

---
+++ api/app.ts
@@ -0,0 +1,24 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+class App {
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+
+    private config(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+            next();
+        });
+    }
+}
+
+export default new App().app;
