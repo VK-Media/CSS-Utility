f046855528c88f82c909cc9a022a28b934e544fe
Robin Louis van Komen <robin@vkmedia.dk>
2019-06-05 16:19:05 +0200

    move style specific logic to style controller

--- api/controllers/StyleController.ts
+++ api/controllers/StyleController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import * as fs from 'fs';
+import * as sass from 'node-sass';
 
 import Files from '../utility/Files';
 
@@ -7,11 +9,36 @@ export class StyleController {
         const outputDirectory: string = './output';
 
         if (!Files.directoryExists(outputDirectory)) {
-            Files.createOutputDirectory(outputDirectory);
+            Files.createDirectory(outputDirectory);
         }
 
-        const success: boolean = Files.renderScss('./styles/styles.scss', './output/styles.css');
+        const success: boolean = this.renderScss('./styles/styles.scss', './output/styles.css');
 
         res.send({ success });
+    }
+
+    protected renderScss(input: string, output: string): boolean {
+        sass.render({
+            file: input,
+            outputStyle: 'compressed'
+        }, (err, result) => {
+            if (err) {
+                console.log(err);
+                return false;
+            }
+
+            fs.writeFile(output, result.css, err => {
+                if (err) {
+                    console.log(err);
+                    return false;
+                }
+            });
+        });
+
+        return true;
+    }
+
+    protected createScssFile() {
+
     }
 }
--- api/utility/Files.ts
+++ api/utility/Files.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as sass from 'node-sass';
 
 class Files {
-    public static createOutputDirectory(path: string): boolean {
+    public static createDirectory(path: string): boolean {
         fs.mkdir(path, { recursive: true }, err => {
             if (err) return false;
         });
@@ -12,31 +12,6 @@ class Files {
 
     public static directoryExists(path: string): boolean {
         return fs.existsSync(path)
-    }
-
-    public static renderScss(input: string, output: string): boolean{
-        sass.render({
-            file: input,
-            outputStyle: 'compressed'
-        }, (err, result) => {
-            if(err){
-                console.log(err);
-                return false;
-            }
-
-            fs.writeFile(output, result.css, err => {
-                if(err){
-                    console.log(err);
-                    return false;
-                }
-            });
-        });
-
-        return true;
-    }
-
-    public static createScssFile(){
-        
     }
 }
 
